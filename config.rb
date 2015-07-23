236dffe98fdee81bc761287cb7f9f8bbc38c494d
sliveredtongue <sliveredtongue@gmail.com>
2015-07-23 13:51:35 -0400

    SCSS to SASS
    Moved project to indented syntax

--- config.rb
+++ config.rb
@@ -21,6 +21,6 @@ relative_assets = true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
